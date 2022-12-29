c446ef716b39d2a9423b02b39521710b6ef69aef
Brijesh Vadaliya <76428127+brijeshvadalia@users.noreply.github.com>
2022-12-29 08:49:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
